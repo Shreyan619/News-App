6477b47a430b9cd93fc7eb18839481550cab49b2
Shreyan619 <shreyankundu73@gmail.com>
2024-08-07 01:14:01 +0530

    started scraping

--- backend/src/scrape/aajtak.acrape.js
+++ backend/src/scrape/aajtak.acrape.js
@@ -11,7 +11,7 @@ export const scrapeArticles = async () => {
         page.setDefaultNavigationTimeout(0);
         await page.goto('https://www.aajtak.in/', { waitUntil: 'networkidle2' });
 
-        const containerSelector = "#badi_khabar_1"; // Define the container selector once
+        const containerSelector = "#badi_khabar_1";
 
         const selectors = [
             {
@@ -23,35 +23,82 @@ export const scrapeArticles = async () => {
             {
                 title: "div > h3 > a",
                 link: "div > h3 > a",
-                image:"div.thumb.video_asso > img",
-                description:"div.single_str > a > div.title > h3"
+                image: "div.thumb.video_asso > img",
+                description: "div.single_str > a > div.title > h3"
             },
-            // Add more selectors as needed
+            {
+                // title: "li:nth-child(1) > a",
+                link: "li:nth-child(1) > a",
+                image: "li:nth-child(1) > a > div.thumb > img",
+                description: "li:nth-child(1) > a > div.title.title-bold > h3"
+            },
+            {
+                // title: "div > h3 > a",
+                link: "li:nth-child(2) > a",
+                image: "li:nth-child(2) > a > div.thumb.video_asso > img",
+                description: "li:nth-child(2) > a > div.title.title-bold > h3"
+            },
+            {
+                // title: "div > h3 > a",
+                link: "li:nth-child(3) > a",
+                image: "li:nth-child(3) > a > div.thumb > img",
+                description: "li:nth-child(3) > a > div.title.title-bold > h3"
+            },
+            {
+                // title: "div > h3 > a",
+                link: "li:nth-child(4) > a",
+                image: "li:nth-child(4) > a > div.thumb.video_asso > img",
+                description: "li:nth-child(4) > a > div.title.title-bold > h3"
+            },
+            {
+                // title: "div > h3 > a",
+                link: "li:nth-child(5) > a",
+                image: "li:nth-child(5) > a > div.thumb > img",
+                description: "li:nth-child(5) > a > div.title.title-bold > h3"
+            },
+            {
+                // title: "div > h3 > a",
+                link: "li:nth-child(6) > a",
+                image: "li:nth-child(6) > a > div.thumb > img",
+                description: "li:nth-child(6) > a > div.title.title-bold > h3"
+            },
+            {
+                // title: "div > h3 > a",
+                link: "li:nth-child(7) > a",
+                image: "li:nth-child(7) > a > div.thumb > img",
+                description: "li:nth-child(7) > a > div.title.title-bold > h3"
+            },
+
         ];
 
         const scrapedData = []
 
         console.log(`Waiting for selector: ${containerSelector}`);
-        await page.waitForSelector(containerSelector, { timeout: 10000 }); // Increased timeout for selector
+        await page.waitForSelector(containerSelector, { timeout: 10000 });
 
         const articles = await page.$$(containerSelector);
 
         for (const article of articles) {
+
             for (const { title, link, image, description } of selectors) {
                 try {
                     const isAttached = await page.evaluate(el => el.isConnected, article);
+
                     if (!isAttached) {
                         console.log("Article element is detached, skipping...");
                         continue;
                     }
 
                     const articleTitle = await article.$eval(title, el => el.getAttribute('title') || el.innerText).catch(() => null);
+
                     const articleLink = await article.$eval(link, el => el.href).catch(() => null);
+
                     const articleImage = image ? await article.$eval(image, el => el.src).catch(() => null) : null;
+
                     const articleDescription = description ? await article.$eval(description, el => el.textContent.trim()).catch(() => null) : null;
 
 
-                    // Check if at least one necessary field is not null before pushing to scrapedData
+
                     if (articleTitle || articleLink || articleImage || articleDescription) {
                         scrapedData.push({
                             title: articleTitle,
@@ -74,5 +121,5 @@ export const scrapeArticles = async () => {
     }
 };
 
-// Ensure the function is called
+
 scrapeArticles();

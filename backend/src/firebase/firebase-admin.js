962ee611ad6adec277d1d812bb959112ea0d6e39
Shreyan619 <shreyankundu73@gmail.com>
2024-09-02 14:33:58 +0530

    ..

--- backend/src/firebase/firebase-admin.js
+++ backend/src/firebase/firebase-admin.js
@@ -1,10 +1,22 @@
 import admin from "firebase-admin"
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" }
+// import serviceAccount from "./serviceAccountKey.json" with { type: "json" }
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "verdant-art-370316.appspot.com"
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  }),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
 export default admin
-export const bucket=admin.storage().bucket()
+export const bucket = admin.storage().bucket()
